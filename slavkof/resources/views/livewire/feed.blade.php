6817cfca88929b54402af3b35b037c1a91d94113
Slavko Fodor <slavko.fodor@gmail.com>
2024-01-20 10:31:13 +0100

    Added TurbineUI

--- slavkof/resources/views/livewire/feed.blade.php
+++ slavkof/resources/views/livewire/feed.blade.php
@@ -1,19 +1,24 @@
 <div>
-    <table class="table-auto">
-        <tr>
+    <x-turbine-ui-table
+        variant="light"
+        divide
+        striped
+    >
+        <x-slot:thead>
             <th>Author</th>
             <th>Message</th>
             <th>Date</th>
             <th>Hash</th>
-        </tr>
+        </x-slot:thead>
+        <x-slot:tbody>
         @foreach($commits as $commit)
-            <tr>
-                <td>{{$commit->getAuthor()}}</td>
+            <tr class="font-mono">
+                <td >{{$commit->getAuthor()}}</td>
                 <td>{{$commit->getMessage()}}</td>
                 <td>{{date('d-m-Y-H-i-s', $commit->getCreatedAt())}}</td>
                 <td>{{$commit->getTree()}}</td>
             </tr>
         @endforeach
-    </table>
-
+        </x-slot:tbody>
+    </x-turbine-ui-table >
 </div>
