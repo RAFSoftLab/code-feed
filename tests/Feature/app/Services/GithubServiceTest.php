6426fa88b40068cf9e4d77a1fd7d4c6ec737ae5d
Slavko Fodor <slavko.fodor@gmail.com>
2024-02-01 11:41:15 +0100

    Added GithubServiceTest.php
    Also removed ExampleTest.php.

--- tests/Feature/app/Services/GithubServiceTest.php
+++ tests/Feature/app/Services/GithubServiceTest.php
@@ -2,20 +2,21 @@
 
 namespace Tests\Feature\app\Services;
 
+use App\Services\GithubService;
 use Tests\TestCase;
 
 class GithubServiceTest extends TestCase
 {
     public function test_get_avatar_from_good_email(): void
     {
-        $githubService = new \App\Services\GithubService();
+        $githubService = new GithubService();
         $avatar = $githubService->loadAvatar('slavko.fodor@gmail.com');
         self::assertEquals('https://avatars.githubusercontent.com/slavkof', $avatar);
     }
 
     public function test_get_avatar_from_bad_email(): void
     {
-        $githubService = new \App\Services\GithubService();
+        $githubService = new GithubService();
         $avatar = $githubService->loadAvatar('bad_email@example.com');
         self::assertEquals('https://www.shutterstock.com/image-vector/default-avatar-profile-icon-vector-600nw-1745180411.jpg', $avatar);
     }
